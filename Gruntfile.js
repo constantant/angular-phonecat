5272494a83a7e9c6c71ae1b9b64b9239a60409be
constantant <k.haritonov@gmail.com>
2015-02-06 13:40:25 +0300

    changed

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,5 @@
 module.exports = function(grunt) {
 
-	// 1. Вся настройка находится здесь
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 
@@ -24,11 +23,9 @@ module.exports = function(grunt) {
 
 	});
 
-	// 3. Тут мы указываем Grunt, что хотим использовать этот плагин
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 
-	// 4. Указываем, какие задачи выполняются, когда мы вводим «grunt» в терминале
 	grunt.registerTask('default', ['concat', 'uglify']);
 
 };
